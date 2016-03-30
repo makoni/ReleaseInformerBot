c1c7308c807e750519d42e1c227d7b86a45ba812
Sergey Armodin <mak@arm1.ru>
2016-03-30 21:28:26 +0300

    - CouchDB settings added to config

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 'use strict';
 
+// CouchDB
+exports.couchDbHost = 'localhost';
+exports.couchDbPort = 5984;
+exports.couchDbDatabase = 'release_bot';
+
 exports.requestTimeout = 1000 * 60 * 30; // 20 seconds for long polling request timeout
 exports.token = '';
