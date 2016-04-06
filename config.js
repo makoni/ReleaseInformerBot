938f25d32a9142246f8cecd7636159a22afaef36
Sergey Armodin <mak@arm1.ru>
2016-04-06 22:23:08 +0300

    Config update

--- config.js
+++ config.js
@@ -6,4 +6,4 @@ exports.couchDbPort = 5984;
 exports.couchDbDatabase = 'release_bot';
 
 exports.requestTimeout = 1000 * 60 * 30; // 20 seconds for long polling request timeout
-exports.token = '';
+exports.token = process.env.TOKEN || 'add-token-here';
