094d9bbc040c9168503ceeb9a4d3437359caaeed
Sergey Armodin <mak@arm1.ru>
2016-04-07 00:02:11 +0300

    bot and watcher moved to lib folder

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ const couch = new nodeCouchDB(config.couchDbHost, config.couchDbPort);
 let telegramBot = new TelegramBot(config.token, {polling: true});
 
 // Release Bot
-const ReleaseBot = require('./release_bot');
+const ReleaseBot = require('./lib/release_bot');
 let releaseBot = new ReleaseBot(telegramBot, couch);
 
 // Watcher to check updates in iTunes
-const ReleasesWatcher = require('./release_watcher');
+const ReleasesWatcher = require('./lib/release_watcher');
 let releaseWatcher = new ReleasesWatcher(releaseBot, couch);
