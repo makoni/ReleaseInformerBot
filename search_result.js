f308d971aeb9611ed5d0d374c4f99ec68d8621b0
Sergey Armodin <mak@arm1.ru>
2016-04-06 22:41:13 +0300

    - SearchResult fix for bundle_id

--- search_result.js
+++ search_result.js
@@ -3,7 +3,7 @@
 class SearchResult {
     constructor(data) {
         this.title = data.title || data.trackName || '';
-    	this.bundleId = data.bundleId || '';
+    	this.bundleId = data.bundleId || data.bundle_id || '';
     	this.url = data.url || data.trackViewUrl || '';
     	this.version = data.version || '';
     }
