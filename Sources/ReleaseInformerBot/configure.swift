090b7f93f87325ea145c14e6bf1e823138b09e0a
Sergey Armodin <dolph-in@yandex.ru>
2025-07-08 23:30:55 +0700

    formatting

--- Sources/ReleaseInformerBot/configure.swift
+++ Sources/ReleaseInformerBot/configure.swift
@@ -11,42 +11,42 @@ let releaseWatcher = ReleaseWatcher(dbManager: dbManager)
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    do {
-        try await dbManager.setupIfNeed()
-    } catch {
-        logger.error("Database setup failed: \(error.localizedDescription)")
-        try await Task.sleep(for: .seconds(10))
-        exit(1)
-    }
-    
-    let bot: TGBot
-    do {
-        bot = try await .init(
-            connectionType: .longpolling(limit: nil, timeout: nil, allowedUpdates: nil),
-            dispatcher: nil,
-            tgClient: AsyncHttpTGClient(),
-            tgURI: TGBot.standardTGURL,
-            botId: ProcessInfo.processInfo.environment["apiKey"] ?? "",
-            log: app.logger
-        )
-    } catch {
-        logger.error("Could not initialize bot: \(error)")
-        try await Task.sleep(for: .seconds(10))
-        exit(1)
-    }
+	do {
+		try await dbManager.setupIfNeed()
+	} catch {
+		logger.error("Database setup failed: \(error.localizedDescription)")
+		try await Task.sleep(for: .seconds(10))
+		exit(1)
+	}
+
+	let bot: TGBot
+	do {
+		bot = try await .init(
+			connectionType: .longpolling(limit: nil, timeout: nil, allowedUpdates: nil),
+			dispatcher: nil,
+			tgClient: AsyncHttpTGClient(),
+			tgURI: TGBot.standardTGURL,
+			botId: ProcessInfo.processInfo.environment["apiKey"] ?? "",
+			log: app.logger
+		)
+	} catch {
+		logger.error("Could not initialize bot: \(error)")
+		try await Task.sleep(for: .seconds(10))
+		exit(1)
+	}
 
 	let botActor: BotActor = .init()
 
 	await botActor.setBot(bot)
 	await BotHandlers.addHandlers(bot: botActor.bot)
 
-    do {
-        try await botActor.bot.start()
-    } catch {
-        logger.error("Could not start bot: \(error.localizedDescription)")
-        try await Task.sleep(for: .seconds(10))
-        exit(1)
-    }
+	do {
+		try await botActor.bot.start()
+	} catch {
+		logger.error("Could not start bot: \(error.localizedDescription)")
+		try await Task.sleep(for: .seconds(10))
+		exit(1)
+	}
 
 	await releaseWatcher.setBot(botActor.bot)
 	await releaseWatcher.start()
